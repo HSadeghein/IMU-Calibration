7ce6fa95e5bf3f90559a1c14ab7518042fdcd46c
Hassan Sadeghein <h.sadeghein96@gmail.com>
2020-01-25 13:58:46 +0200

    Least Square

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-
+import math
+import numpy as np
 # plotting the measurement data
 
 
@@ -16,12 +17,16 @@ def plot_measurements():
                       margin=dict(l=65, r=50, b=65, t=90))
     fig.show()
 
+# plotting the measurement and groundtruth
+
 
 def plot_together():
     df_m = pd.read_csv('./data/measurements.csv',
                        usecols=[0, 1, 2], header=None, index_col=None)
     df_gt = pd.read_csv('./data/groundtruth.csv',
                         usecols=[0, 1, 2], header=None, index_col=None)
+    df_estimated = pd.read_csv('./data/estimated.csv',
+                               usecols=[0, 1, 2], header=None, index_col=None)
     data_m = go.Scatter3d(
         x=df_m[df_m.columns[0]], y=df_m[df_m.columns[1]], z=df_m[df_m.columns[2]], mode='markers', marker=dict(
             size=12,
@@ -43,7 +48,20 @@ def plot_together():
             ),
             opacity=0.9
         ), name="Ground Truth")
-    data = [data_m, data_gt]
+
+    data_estimated = go.Scatter3d(
+        x=df_estimated[df_estimated.columns[0]], y=df_gt[df_estimated.columns[1]], z=df_estimated[df_estimated.columns[2]], mode='markers', marker=dict(
+            color='rgb(180, 120, 0)',
+            size=12,
+            symbol='circle',
+            line=dict(
+                color='rgb(204, 204, 204)',
+                width=1
+            ),
+            opacity=0.9
+        ), name="Estimated")
+
+    data = [data_m, data_gt, data_estimated]
     layout = go.Layout(
         margin=dict(
             l=0,
@@ -57,7 +75,31 @@ def plot_together():
     fig.show()
 
 
+def calc_least_square():
+    df_m = pd.read_csv('./data/measurements.csv',
+                       usecols=[0, 1, 2, 3], header=None, index_col=None)
+    df_gt = pd.read_csv('./data/groundtruth.csv',
+                        usecols=[0, 1, 2, 3], header=None, index_col=None)
+
+    # e = df_gt.subtract(df_m).pow(2)
+    # e = e[0] + e[1] + e[2]
+    # e = e.sum(axis=0, skipna=True)
+    # print(e)
+    m_list = df_m.to_numpy()
+    gr_list = df_gt.to_numpy()
+    m_list_tp = np.transpose(m_list)
+    # A^T * A X` = A^T * GroundTruth
+    # X` = (A^T*A)^-1 * A^T * GroundTruth
+    estimated_matrix = np.matmul(np.matmul(np.linalg.inv(
+        np.matmul(m_list_tp, m_list)), m_list_tp), gr_list)
+    print(estimated_matrix)
+    df_estimated = pd.DataFrame(np.matmul(m_list, estimated_matrix))
+    df_estimated.to_csv("./data/estimated.csv", index=False, header=False)
+
+
 def main():
+    # plot_together()
+    # calc_least_square()
     plot_together()
 
 
